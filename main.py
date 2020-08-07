883599e6c35da3c9e9bd1f8ac1e87f496dadd2b2
vedantdesai11 <vedantpratikdesai@gmail.com>
2020-08-06 23:14:35 -0500

    Progress

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 env = gym.make("MountainCar-v0")
 
 learning_rate = 0.1
 discount = 0.95
-episodes = 2500
+episodes = 25000
 
-show = 500
+show = 1000
 
 discrete_os_size = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / discrete_os_size
@@ -22,7 +25,7 @@ epsilon_decay_value = epsilon / (end_epsilon_decay - start_epsilon_decay)
 q_table = np.random.uniform(low=-2, high=0, size=(discrete_os_size + [env.action_space.n]))
 
 episode_rewards = []
-aggrigate_episode_rewards = {'ep':[], 'avg': [], 'min': [], 'max': []}
+aggrigate_episode_rewards = {'ep': [], 'avg': [], 'min': [], 'max': []}
 
 
 def get_discrete_state(state):
@@ -31,12 +34,15 @@ def get_discrete_state(state):
 
 
 for episode in range(episodes):
+
 	episode_reward = 0
+	"""
 	if episode % show == 0:
-		print(episode)
 		render = True
 	else:
 		render = False
+	"""
+	render = False
 
 	discrete_state = get_discrete_state(env.reset())
 
@@ -58,15 +64,15 @@ for episode in range(episodes):
 			env.render()
 
 		if not done:
-			max_future_q =  np.max(q_table[new_discrete_state])
-			current_q = q_table[discrete_state + (action, )]
+			max_future_q = np.max(q_table[new_discrete_state])
+			current_q = q_table[discrete_state + (action,)]
 
-			new_q = (1-learning_rate) *  current_q + learning_rate * (reward + discount * max_future_q)
+			new_q = (1 - learning_rate) * current_q + learning_rate * (reward + discount * max_future_q)
 
-			q_table[discrete_state + (action, )] = new_q
+			q_table[discrete_state + (action,)] = new_q
 
 		elif new_state[0] >= env.goal_position:
-			print(f"We made it on episode {episode}")
+			#print(f"We made it on episode {episode}")
 			q_table[discrete_state + (action,)] = 0
 
 		discrete_state = new_discrete_state
@@ -76,15 +82,13 @@ for episode in range(episodes):
 
 	episode_rewards.append(episode_reward)
 
-	if not episode % show:
+	if episode % 10 == 0:
 		average_reward = sum(episode_rewards[-show:]) / len(episode_rewards[-show:])
 		aggrigate_episode_rewards['ep'].append(episode)
 		aggrigate_episode_rewards['avg'].append(average_reward)
 		aggrigate_episode_rewards['min'].append(min(episode_rewards[-show:]))
 		aggrigate_episode_rewards['max'].append(max(episode_rewards[-show:]))
-
-		print(f"Episode:{episode}, Average:{average_reward}, Minimun:{min(episode_rewards[-show:])}, Maximum:{max(episode_rewards[-show:])}")
-
+		np.save(f"qtables/{episode}-qtable.npy", q_table)
 
 env.close()
 
@@ -92,5 +96,9 @@ plt.plot(aggrigate_episode_rewards['ep'], aggrigate_episode_rewards['avg'], labe
 plt.plot(aggrigate_episode_rewards['ep'], aggrigate_episode_rewards['min'], label="Minimun")
 plt.plot(aggrigate_episode_rewards['ep'], aggrigate_episode_rewards['max'], label="Maximum")
 
-plt.legend(loc = 4)
-plt.show()
+plt.legend(loc=2)
+plt.savefig(f"Episode_VS_Rewards.png")
+
+
+
+
